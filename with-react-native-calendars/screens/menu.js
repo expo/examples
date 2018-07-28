c49756e584c0b36f638a1b2ee50ea3b7a3dc4c7b
Caleb Nance <calebnance@gmail.com>
2018-07-28 11:18:46 -0400

    upgrade `with-react-native-calendars` to Expo SDK 29

--- with-react-native-calendars/screens/menu.js
+++ with-react-native-calendars/screens/menu.js
@@ -7,17 +7,22 @@ export default class MenuScreen extends Component {
       <View>
         <TouchableOpacity
           style={styles.menu}
-          onPress={this.onCalendarsPress.bind(this)}>
+          onPress={this.onCalendarsPress.bind(this)}
+        >
           <Text style={styles.menuText}>Calendars</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
           style={styles.menu}
-          onPress={this.onCalendarListPress.bind(this)}>
+          onPress={this.onCalendarListPress.bind(this)}
+        >
           <Text style={styles.menuText}>Calendar List</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
           style={styles.menu}
-          onPress={this.onAgendaPress.bind(this)}>
+          onPress={this.onAgendaPress.bind(this)}
+        >
           <Text style={styles.menuText}>Agenda</Text>
         </TouchableOpacity>
       </View>
