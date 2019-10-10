58ef6289bead2725b162b766c98b3468a5238617
Brooke Mitchell <brooke.mitchell@vetradar.com>
2019-10-10 13:02:08 +1300

    fix(with-storybook): Stop font story loading till async font loaded

--- with-storybook/stories/4-Font.stories.js
+++ with-storybook/stories/4-Font.stories.js
@@ -13,23 +13,25 @@ export function font() {
   useEffect(() => {
     (async () => {
       await Font.loadAsync({
-        'retro-regular': require('../assets/retro-regular.ttf'),
+        'retro-regular': require('../assets/retro-regular.ttf')
       });
       setLoaded(true);
     })();
   }, []);
 
   return (
-    <Text
-      style={{
-        fontFamily: 'retro-regular',
-        backgroundColor: 'transparent',
-        fontSize: 56,
-        color: '#000',
-      }}
-    >
-      Cool new font
-    </Text>
+    loaded && (
+      <Text
+        style={{
+          fontFamily: 'retro-regular',
+          backgroundColor: 'transparent',
+          fontSize: 56,
+          color: '#000'
+        }}
+      >
+        Cool new font
+      </Text>
+    )
   );
 }
 
