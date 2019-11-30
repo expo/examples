c34d45a1c3de1749eedab470a000fbe466873287
Evan Bacon <baconbrix@gmail.com>
2019-11-29 16:09:48 -0800

    reset project

--- with-nextjs/babel.config.js
+++ with-nextjs/babel.config.js
@@ -1,12 +1,6 @@
-module.exports = function (api) {
-  // Detect web usage (this may change in the future if Next.js changes the loader to `next-babel-loader`)
-  const isWeb = api.caller(caller => caller && caller.name === 'babel-loader');
-
+module.exports = function(api) {
+  api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      // Only use next in the browser, it'll break your native project/
-      isWeb && 'next/babel',
-    ].filter(Boolean),
+    presets: ['babel-preset-expo'],
   };
 };
