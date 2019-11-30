0aeee57a2341b5e6c6ffa639a18eea34f805db3e
Evan Bacon <baconbrix@gmail.com>
2019-11-29 16:25:45 -0800

    updated new structure

--- with-nextjs/next.config.js
+++ with-nextjs/next.config.js
@@ -1,7 +1,12 @@
-// @generated: @expo/next-adapter@2.0.0-beta.7
-// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
-
 const { withExpo } = require('@expo/next-adapter');
+const withImages = require('next-images');
+const withFonts = require('next-fonts');
+const withOffline = require('next-offline');
+
+module.exports = withExpo(withOffline(withFonts(withImages({
+    projectRoot: __dirname,
+    workboxOpts: {
+        swDest: 'workbox-service-worker.js',
 
         /* changing any value means you'll have to copy over all the defaults  */
         /* next-offline */
