4e2eaa3ffbc09c0a024351b1fbc9390ccc561bd6
Evan Bacon <baconbrix@gmail.com>
2020-01-20 14:09:13 -0800

    Created preact example

---
+++ with-preact/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Demo Preact + Expo web</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
