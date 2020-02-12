92a28b80a1d02858e30d895ca6620ddf50e633e5
Cedric van Putten <me@bycedric.com>
2020-02-12 22:50:42 +0000

    feat: add firebase database example

---
+++ with-firebase/App.js
@@ -0,0 +1,121 @@
+import * as firebase from 'firebase';
+import React from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmG9vvm5PUzVYxV_QR0KFOGyZ9sE8kKBo",
+  authDomain: "fir-test-c3482.firebaseapp.com",
+  databaseURL: "https://fir-test-c3482.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "817842099542"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends React.Component {
+  state = {
+    text: '',
+    loading: false,
+  }
+
+  componentWillMount() {
+    let initialLoad = true;
+    this.setState({loading: true});
+
+    firebase.database().ref('example').on('value', (snapshot) => {
+      this.setState({text: snapshot.val() && snapshot.val().text});
+
+      if (initialLoad) {
+        this.setState({loading: false});
+        initialLoad = false;
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Store some value in Firebase!
+        </Text>
+
+        <TextInput
+          onChangeText={text => { this.setState({text}) }}
+          onSubmitEditing={this._saveValue}
+          value={this.state.text}
+          style={styles.textInput}
+        />
+
+        <Button
+          onPress={this._saveValue}
+          title="Save"
+        />
+
+        {this._maybeRenderLoadingOverlay()}
+      </View>
+    );
+  }
+
+  _saveValue = async () => {
+    try {
+      this.setState({loading: true});
+      await firebase.database().ref('example').set({ text: this.state.text });
+    } catch(e) {
+      // Error! oh no
+      console.log(e);
+      alert('Oh no, couldn\'t store your input');
+    } finally {
+      this.setState({loading: false});
+    }
+  }
+
+  _maybeRenderLoadingOverlay = () => {
+    if (this.state.loading) {
+      return (
+        <View style={[StyleSheet.absoluteFill, styles.loadingOverlay]}>
+          <ActivityIndicator
+            color="#fff"
+            animating
+            size="large"
+          />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+  textInput: {
+    width: Dimensions.get('window').width - 30,
+    marginHorizontal: 15,
+    padding: 5,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#eee',
+    marginVertical: 15,
+    height: 50,
+    fontSize: 16,
+  },
+  loadingOverlay: {
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ with-firebase/README.md
@@ -0,0 +1,24 @@
+# Firebase Example
+
+<p>
+  <!-- iOS -->
+  <img alt="Supports Expo iOS" longdesc="Supports Expo iOS" src="https://img.shields.io/badge/iOS-4630EB.svg?style=flat-square&logo=APPLE&labelColor=999999&logoColor=fff" />
+  <!-- Android -->
+  <img alt="Supports Expo Android" longdesc="Supports Expo Android" src="https://img.shields.io/badge/Android-4630EB.svg?style=flat-square&logo=ANDROID&labelColor=A4C639&logoColor=fff" />
+  <!-- Web -->
+  <img alt="Supports Expo Web" longdesc="Supports Expo Web" src="https://img.shields.io/badge/web-4630EB.svg?style=flat-square&logo=GOOGLE-CHROME&labelColor=4285F4&logoColor=fff" />
+</p>
+
+This example demonstrates how you can save some text to Firebase Database. When you save the input value it becomes visible in Firebase.
+
+## 🚀 How to use
+
+- Run `yarn` or `npm install`
+- Run [`expo start`](https://docs.expo.io/versions/latest/workflow/expo-cli/), try it out.
+- Type something you want to save
+- Press save and see your input in Firebase!
+
+## 📝 Notes
+
+- [Firebase JS docs](https://firebase.google.com/docs/web/setup)
+- [Expo Firebase guide](https://docs.expo.io/versions/latest/guides/using-firebase/)
