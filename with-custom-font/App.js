b790b6f0540255708351598cbe3f64de3c72e82b
Charlie Cheever <ccheever@gmail.com>
2020-02-29 19:50:25 -0800

    Streamlining in response to Evan's feedback and contribution guidelines

--- with-custom-font/App.js
+++ with-custom-font/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import { useFonts } from '@use-expo/font';
-import { Text, View, StyleSheet } from 'react-native';
-import { AppLoading } from 'expo';
+import { Text, View, StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 let customFonts = {
-  'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
-  'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+  "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
+  "Inter-SemiBoldItalic":
+    "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12"
 };
 
-export default (props) => {
+export default props => {
   let [fontsLoaded] = useFonts(customFonts);
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Platform Default</Text>
-        <Text style={{ fontFamily: 'Inter-Black' }}>Inter Black</Text>
-        <Text style={{ fontFamily: 'Inter-SemiBoldItalic' }}>Inter SemiBoldItalic</Text>
+        <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
+        <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
+          Inter SemiBoldItalic
+        </Text>
       </View>
     );
   }
 };
+
+function useFonts(fontMap) {
+  let [fontsLoaded, setFontsLoaded] = useState(false);
+  (async () => {
+    await Font.loadAsync(fontMap);
+    setFontsLoaded(true);
+  })();
+  return [fontsLoaded];
+}
