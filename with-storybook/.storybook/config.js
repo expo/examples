dee1b7bce62e38f02726e1ae10c429e11ad4a78b
Evan Bacon <baconbrix@gmail.com>
2020-03-29 15:06:53 -0700

    Upgraded docs and packages

--- with-storybook/.storybook/config.js
+++ with-storybook/.storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure(require.context("../stories", true, /\.stories\.js$/), module);
