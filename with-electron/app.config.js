77b8319ece1c94352b97d268fd422a4669ea44d6
Evan Bacon <baconbrix@gmail.com>
2020-05-12 14:44:00 -0700

    Use app.config.js

---
+++ with-electron/app.config.js
@@ -0,0 +1,12 @@
+export default {
+  name: "with-electron",
+  version: "1.0.0",
+  icon:
+    "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/icon.png?raw=true",
+  splash: {
+    image:
+      "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/splash.png?raw=true",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+};
---
+++ with-postpublish-hooks/app.config.js
@@ -0,0 +1,18 @@
+export default {
+  hooks: {
+    postPublish: [
+      {
+        file: "./hooks/echo",
+        config: {
+          message: "hello brent",
+        },
+      },
+      {
+        file: "expo-postpublish-slack-notify",
+        config: {
+          webhookUrl: "https://hooks.slack.com/your-webhook-info-here",
+        },
+      },
+    ],
+  },
+};
