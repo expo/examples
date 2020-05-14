2f3ce764ecbb3323d9f5b699999f31490906c59e
Faraz Patankar <farazpatankar@gmail.com>
2020-05-14 18:51:32 -0400

    add comment to explain authentication setup

--- with-apollo/apollo.js
+++ with-apollo/apollo.js
@@ -2,15 +2,23 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 // import { setContext } from '@apollo/link-context';
 
 const GRAPHQL_API_URL = 'https://www.graphqlhub.com/graphql';
-// const TOKEN = '';
 
-// const asyncAuthLink = setContext(async () => {
-//   return {
-//     headers: {
-//       Authorization: TOKEN,
-//     },
-//   };
-// });
+/*
+uncomment the code below in case you are using a GraphQL API that requires some form of
+authentication. asyncAuthLink will run every time your request is made and use the token
+you provide while making the request.
+
+
+const TOKEN = '';
+const asyncAuthLink = setContext(async () => {
+  return {
+    headers: {
+      Authorization: TOKEN,
+    },
+  };
+});
+
+*/
 
 const httpLink = new HttpLink({
   uri: GRAPHQL_API_URL,
@@ -19,6 +27,5 @@ const httpLink = new HttpLink({
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: httpLink,
-  // use this when using with auth token
   // link: asyncAuthLink.concat(httpLink), 
 });
