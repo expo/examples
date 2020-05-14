f63cd754871c6b208f80d2731a6af6a17362120b
Evan Bacon <baconbrix@gmail.com>
2020-05-14 10:19:41 -0700

    Created shoutem example

---
+++ with-shoutem/App.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import { StatusBar } from "react-native";
+import { View, Examples } from "@shoutem/ui";
+
+export default function App() {
+  return (
+    <View styleName="flexible">
+      <Examples />
+      <StatusBar barStyle="default" hidden={false} />
+    </View>
+  );
+}
