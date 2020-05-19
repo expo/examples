4cc1929291ee87d4142f158f7e1e89a5b653749b
Faraz Patankar <farazpatankar@gmail.com>
2020-05-18 20:37:34 -0400

    add with-drawer-navigation example

---
+++ with-drawer-navigation/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={navigation.openDrawer}
+        title="Open navigation drawer"
+      />
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={navigation.openDrawer}
+        title="Open navigation drawer"
+      />
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Go back home"
+      />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
