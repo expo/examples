9caae72b1052b8bf58f0d14d480dfd370a63263f
Brent Vatne <brentvatne@gmail.com>
2020-12-22 16:23:31 -0800

    Update with-webbrowser-redirect/App.js
    Co-authored-by: Evan Bacon <baconbrix@gmail.com>

--- with-webbrowser-redirect/App.js
+++ with-webbrowser-redirect/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import * as Linking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
