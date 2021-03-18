50a8e0746570f2794c4958bced782f1d36613e34
Evan Bacon <baconbrix@gmail.com>
2021-03-17 17:40:14 -0700

    updated scripts and added continueUserActivity

--- with-development-client/ios/HelloWorld/AppDelegate.m
+++ with-development-client/ios/HelloWorld/AppDelegate.m
@@ -123,6 +123,13 @@ static void InitializeFlipper(UIApplication *application) {
   [splashScreenService showSplashScreenFor:self.window.rootViewController];
 }
 
+// Universal Links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+
 - (BOOL)application:(UIApplication *)application
    openURL:(NSURL *)url
    options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
