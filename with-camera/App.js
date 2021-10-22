664f65c94d64bea87e7ad328cd9a13910e94145e
Cedric van Putten <me@bycedric.com>
2021-10-23 01:24:51 +0200

    [camera] Upgrade to Expo SDK 43 and use permission hook

--- with-camera/App.js
+++ with-camera/App.js
@@ -1,17 +1,21 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  ImageBackground,
-} from "react-native";
+import React, { useState, useRef } from "react";
+import { Button, Text, View, TouchableOpacity, ImageBackground } from "react-native";
 import { Camera } from "expo-camera";
-import CameraPermissionsWrapper from './CameraPermissionsWrapper';
 
 export default function App() {
+  const [status, requestPermission] = Camera.useCameraPermissions();
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [lastPhotoURI, setLastPhotoURI] = useState(null);
   const cameraRef = useRef(null);
+
+  if (!status?.granted) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+        <Text style={{ textAlign: 'center' }}>We need access to your camera</Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
 
   if (lastPhotoURI !== null) {
     return (
@@ -45,59 +49,57 @@ export default function App() {
   }
 
   return (
-    <CameraPermissionsWrapper>
-      <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
-        <View
+    <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "transparent",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
           style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row",
+            flex: 0.2,
+            alignSelf: "flex-end",
+            alignItems: "center",
             justifyContent: "center",
+            backgroundColor: "#666",
+            marginBottom: 40,
+            marginLeft: 20,
+          }}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
           }}
         >
-          <TouchableOpacity
-            style={{
-              flex: 0.2,
-              alignSelf: "flex-end",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#666",
-              marginBottom: 40,
-              marginLeft: 20,
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          >
-            <Text style={{ fontSize: 30, padding: 10, color: "white" }}>♻</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flex: 0.2,
-              alignSelf: "flex-end",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#666",
-              marginBottom: 40,
-              marginLeft: 20,
-            }}
-            onPress={async () => {
-              if (cameraRef.current) {
-                let photo = await cameraRef.current.takePictureAsync();
-                setLastPhotoURI(photo.uri);
-              }
-            }}
-          >
-            <Text style={{ fontSize: 30, padding: 10, color: "white" }}>
-              📸
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </CameraPermissionsWrapper>
+          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>♻</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            flex: 0.2,
+            alignSelf: "flex-end",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#666",
+            marginBottom: 40,
+            marginLeft: 20,
+          }}
+          onPress={async () => {
+            if (cameraRef.current) {
+              let photo = await cameraRef.current.takePictureAsync();
+              setLastPhotoURI(photo.uri);
+            }
+          }}
+        >
+          <Text style={{ fontSize: 30, padding: 10, color: "white" }}>
+            📸
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
   );
 }
