3d4981730640b5ce9cca3afe487751bba7a2309e
Brent Vatne <brentvatne@gmail.com>
2021-10-22 12:29:11 -0700

    Revert "[with-nextjs] upgrade to Next.js 11"
    This reverts commit 69d784afbdcb3d1bbdeb9d4a1a3232053c5469f8.

--- with-nextjs/next.config.js
+++ with-nextjs/next.config.js
@@ -1,10 +1,8 @@
+// @generated: @expo/next-adapter@2.1.52
+// Learn more: https://docs.expo.io/guides/using-nextjs/
+
 const { withExpo } = require('@expo/next-adapter');
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(['react-native-web']);
 
-const nextConfig = {};
-
-module.exports = withPlugins(
-  [withTM, [withExpo, { projectRoot: __dirname }]],
-  nextConfig
-);
+module.exports = withExpo({
+  projectRoot: __dirname,
+});
