e265a59ec971590433f9aa168d86f5d6e12892ae
Evan Bacon <baconbrix@gmail.com>
2021-12-14 13:00:07 -0700

    Created example for react-native-elements (#347)

---
+++ with-react-native-elements/App.js
@@ -0,0 +1,55 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, CheckBox } from "react-native-elements";
+
+export default function AppContainer() {
+  const [ready, setReady] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        await Font.loadAsync(MaterialIcons.font);
+      } catch (e) {
+        // error caching font, not a big deal, will load on the fly
+      } finally {
+        setReady(true);
+      }
+    })();
+  }, []);
+
+  if (ready) {
+    return <App />;
+  }
+  return <AppLoading />;
+}
+
+function App() {
+  const [checked, setChecked] = React.useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Button
+        raised
+        icon={{ name: "cached", color: "white" }}
+        title="RAISED WITH ICON"
+      />
+      <CheckBox
+        title="Click Here"
+        checked={checked}
+        onPress={() => setChecked((checked) => !checked)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ with-react-native-elements/README.md
@@ -0,0 +1,31 @@
+# `react-native-elements` Example
+
+<p>
+  <!-- iOS -->
+  <a href="https://itunes.apple.com/app/apple-store/id982107779">
+    <img alt="Supports Expo iOS" longdesc="Supports Expo iOS" src="https://img.shields.io/badge/iOS-4630EB.svg?style=flat-square&logo=APPLE&labelColor=999999&logoColor=fff" />
+  </a>
+  <!-- Android -->
+  <a href="https://play.google.com/store/apps/details?id=host.exp.exponent&referrer=blankexample">
+    <img alt="Supports Expo Android" longdesc="Supports Expo Android" src="https://img.shields.io/badge/Android-4630EB.svg?style=flat-square&logo=ANDROID&labelColor=A4C639&logoColor=fff" />
+  </a>
+  <!-- Web -->
+  <a href="https://docs.expo.dev/workflow/web/">
+    <img alt="Supports Expo Web" longdesc="Supports Expo Web" src="https://img.shields.io/badge/web-4630EB.svg?style=flat-square&logo=GOOGLE-CHROME&labelColor=4285F4&logoColor=fff" />
+  </a>
+</p>
+
+Example demonstrating use of the [`react-native-elements`][rne] library in Expo.
+
+![Simulator Example](https://i.imgur.com/CDrXKzV.png)
+
+## 🚀 How to use
+
+- Run `yarn` or `npm install`
+- Run [`expo start`](https://docs.expo.dev/versions/latest/workflow/expo-cli/), try it out.
+
+## 📝 Notes
+
+- [React Native Elements docs][rne]
+
+[rne]: https://reactnativeelements.com/
