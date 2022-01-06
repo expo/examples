5fd129c1bd62cf57cb8e3b83feb0a7d2cc5383a9
Andrew Meyer <andrew.meyer@mongodb.com>
2022-01-06 20:21:51 +0100

    Add with-realm example (#351)
    With the release of SDK 44 and the `expo-dev-client`, `realm` can
    finally work with Expo!  This example is based off of the templates
    already existing in [Realm-Js](https://github.com/realm/realm-js).

---
+++ with-realm/index.js
@@ -0,0 +1,11 @@
+import 'expo-dev-client';
+import 'react-native-get-random-values';
+
+import { registerRootComponent } from "expo";
+
+import App from "./App";
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
