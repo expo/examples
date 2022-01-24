c1ed3dec2f3c263ebae97262ea5506192f3d7b9c
Evan Bacon <baconbrix@gmail.com>
2022-01-24 13:59:22 -0700

    Create react-native-pdf Example (#360)
    * Created with-pdf example
    
    * Updated packages

---
+++ with-pdf/App.js
@@ -0,0 +1,32 @@
+import { SafeAreaView, useWindowDimensions } from "react-native";
+import Pdf from "react-native-pdf";
+
+const source = {
+  uri: "https://samples.leanpub.com/thereactnativebook-sample.pdf",
+  cache: true,
+};
+
+export default function App() {
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={(error) => {
+          console.log(error);
+        }}
+        onPressLink={(uri) => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={{ flex: 1, width, height }}
+      />
+    </SafeAreaView>
+  );
+}
