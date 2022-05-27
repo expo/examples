b496646083f020e016368f8b9dd4a8e7328677e1
Kim Brandwijk <kim.brandwijk@gmail.com>
2022-05-28 01:09:18 +0200

    SDK45 (#371)

--- with-custom-font/App.js
+++ with-custom-font/App.js
@@ -1,6 +1,7 @@
-import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
 import { Text, View } from "react-native";
+import { useEffect, useCallback } from "react";
 
 export default function App() {
   const [fontsLoaded] = Font.useFonts({
@@ -9,12 +10,28 @@ export default function App() {
       "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12",
   });
 
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View 
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      onLayout={onLayoutRootView}>
       <Text>Platform Default</Text>
       <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
       <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
