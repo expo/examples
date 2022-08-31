92a2ef23ba99d636a4cb33d0a558eb7785913bd9
Simek <gosimek@gmail.com>
2022-08-31 15:14:45 +0200

    [with-nextjs] bump Next, update configs

--- with-nextjs/next.config.js
+++ with-nextjs/next.config.js
@@ -1,8 +1,11 @@
-// @generated: @expo/next-adapter@2.1.52
 // Learn more: https://docs.expo.io/guides/using-nextjs/
 
 const { withExpo } = require('@expo/next-adapter');
+const withTM = require('next-transpile-modules')(['react-native-web']);
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-});
+module.exports = () => {
+  const plugins = [withTM, withExpo];
+  return plugins.reduce((acc, next) => next(acc), {
+    // Next Config
+  });
+};
--- with-nextjs/pages/_document.js
+++ with-nextjs/pages/_document.js
@@ -1,2 +1,1 @@
-// @generated: @expo/next-adapter@2.1.52
 export { default } from '@expo/next-adapter/document';
