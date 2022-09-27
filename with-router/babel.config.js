62c1e100a407924539c1e4fe3d17474fd2c3bc89
evanbacon <bacon@expo.io>
2022-09-27 15:43:05 -0500

    created router example

---
+++ with-router/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+    ],
+  };
+};
