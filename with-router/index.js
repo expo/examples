d2c6d15ae702c51fb15dd599b71f48e84bd57175
evanbacon <bacon@expo.io>
2022-09-28 17:41:45 -0500

    update with-router to use the latest

--- with-router/index.js
+++ with-router/index.js
@@ -1,12 +1,1 @@
-// NOTE(EvanBacon): This file will go away in the future.
-import "@bacons/expo-metro-runtime";
-import { registerRootComponent } from "expo";
-import { ExpoRoot } from "expo-router";
-
-// Must be exported or Fast Refresh won't update the context
-export function App() {
-  const ctx = require.context("./app");
-  return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
+import "expo-router/entry";
