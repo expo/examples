67f169530a44167ba45821d54a9693bb06b3685c
Avi Avinav <54372449+AviAvinav@users.noreply.github.com>
2022-11-10 21:34:53 +0530

    add with-nativewind example (#390)

---
+++ with-nativewind/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Universal React with Expo</Text>
+    </View>
+  );
+}
