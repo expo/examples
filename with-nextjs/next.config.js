027677bc8d59832f2fce3a36143731382732a93a
Keith Kurak <keith.kurak@gmail.com>
2022-11-10 10:13:16 -0500

    Update Examples to SDK 47 (#396)
    * add warning to upgrade script
    
    * ran upgrade script on all examples
    
    * remove electron example
    
    per https://linear.app/expo/issue/ENG-6088/upgrade-expo-electron-adapter-to-work-with-recent-sdks
    
    * upgrade nav deps, fix double header
    
    * update next, three examples
    
    * upgrade more deps
    
    * regenerate dev client example native files

--- with-nextjs/next.config.js
+++ with-nextjs/next.config.js
@@ -1,8 +1,8 @@
 // @generated: @expo/next-adapter@2.1.52
 // Learn more: https://docs.expo.io/guides/using-nextjs/
 
-const { withExpo } = require('@expo/next-adapter');
+const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")(["react-native-web"]);
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-});
+module.exports = withPlugins([withTM, withExpo], {});
