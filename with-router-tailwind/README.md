8bc9c9171c087774a25b49cd4b3b5de88a322931
evanbacon <bacon@expo.io>
2023-12-27 21:52:03 +0100

    router with tailwind demo

---
+++ with-router-tailwind/README.md
@@ -0,0 +1,9 @@
+# Expo Router and Tailwind CSS
+
+Use [Expo Router](https://docs.expo.dev/router/introduction/) with [Nativewind](https://www.nativewind.dev/v4/overview/) styling.
+
+## 🚀 How to use
+
+```sh
+npx create-expo-app -e with-router-tailwind
+```
---
+++ with-router-tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
