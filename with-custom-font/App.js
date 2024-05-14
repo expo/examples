98bc5c8de965b278eec2334f3aee0f7150b801b2
Brent Vatne <brentvatne@gmail.com>
2024-05-14 10:29:07 -0700

    Update font example

--- with-custom-font/App.js
+++ with-custom-font/App.js
@@ -1,38 +1,25 @@
 import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { Text, View } from "react-native";
-import { useCallback } from "react";
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import { ActivityIndicator, Text, View } from "react-native";
 
 export default function App() {
   const [fontsLoaded, fontError] = Font.useFonts({
     "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
-    "Inter-SemiBoldItalic":
-      "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12",
   });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator />
+      </View>
+    );
   }
 
+
   return (
-    <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      onLayout={onLayoutRootView}
-    >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Platform Default</Text>
       <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
-      <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
-        Inter SemiBoldItalic
-      </Text>
     </View>
   );
 }
