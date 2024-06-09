67010dee8eb6622ed31d0b45bc6933aa52a735a0
evanbacon <bacon@expo.io>
2024-06-09 17:00:22 +0200

    add example app

---
+++ with-react-compiler/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Text } from "react-native";
+
+function Expensive() {
+  let i = 0;
+  while (i < 1000000000) {
+    i++;
+  }
+
+  return <Text>Rendered: {new Date().toLocaleTimeString()}</Text>;
+}
+
+export default function Page() {
+  const [index, setIndex] = useState(0);
+  return (
+    <>
+      <Text onPress={() => setIndex((i) => i + 1)}>Increment {index}</Text>
+      <Expensive />
+    </>
+  );
+}
