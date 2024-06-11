0510ac99166ff05ae9cc8999e1d5c722af1f7c21
Evan Bacon <bacon@expo.io>
2024-06-11 19:38:28 +0200

    feat: add react compiler example (#485)
    Depends on this landing and getting published
    https://github.com/expo/expo/pull/29168

---
+++ with-react-compiler/.eslintrc.js
@@ -0,0 +1,8 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: "expo",
+  plugins: ["eslint-plugin-react-compiler"],
+  rules: {
+    "react-compiler/react-compiler": "error",
+  },
+};
