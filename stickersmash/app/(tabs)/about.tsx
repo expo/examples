b12d232cf63293eab2bfd07019b7b5f3b657a526
Aman Mittal <amandeepmittal@live.com>
2024-09-10 18:37:05 +0530

    Add stickersmash project

---
+++ stickersmash/app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
---
+++ stickersmash/components/IconButton.tsx
@@ -0,0 +1,28 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type Props = {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  label: string;
+  onPress: () => void;
+};
+
+export default function IconButton({ icon, label, onPress }: Props) {
+  return (
+    <Pressable style={styles.iconButton} onPress={onPress}>
+      <MaterialIcons name={icon} size={24} color="#fff" />
+      <Text style={styles.iconButtonLabel}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconButtonLabel: {
+    color: "#fff",
+    marginTop: 12,
+  },
+});
