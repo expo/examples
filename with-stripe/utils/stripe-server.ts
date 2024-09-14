9236bfdb21d094c197371be5e2d7488eb9ff6d97
evanbacon <bacon@expo.io>
2024-09-14 14:54:35 +0100

    Update stripe-server.ts

--- with-stripe/utils/stripe-server.ts
+++ with-stripe/utils/stripe-server.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  // This is required for deployment.
   httpClient: Stripe.createFetchHttpClient(),
 });
