1b0d96c61c415904b26351e91713a870a9e717e6
Douglas Lowder <douglowder@mac.com>
2024-09-23 23:42:38 -0700

    Improvements to Maestro example (#513)
    - Remove unused app screen
    - Add separate Maestro flows for Expo Go and development builds
    (different app IDs)
    - Add instructions for local testing to `README.md`
    - Add missing app scheme to `app.json`
    - Remove assets and minimize number of files for better maintainability

--- with-maestro/app/(tabs)/_layout.tsx
+++ with-maestro/app/(tabs)/_layout.tsx
@@ -1,35 +1,32 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#eeeeee',
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+          tabBarIcon: () => null,
+          tabBarLabelStyle: {
+            fontSize: 18,
+          },
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          tabBarIcon: () => null,
+          tabBarLabelStyle: {
+            fontSize: 18,
+          },
         }}
       />
     </Tabs>
