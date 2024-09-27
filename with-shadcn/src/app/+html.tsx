a0e2ea76aeb176b9db03991b50c3738c6e09d66c
Evan Bacon <bacon@expo.io>
2024-09-27 16:00:16 +0200

    add shadcn demo (#517)
    # Why
    
    Shadcn CLI no longer aids in the setup of Expo Router projects since it
    isn't one of the recognized frameworks. So I'm adding a slightly
    opinionated example to make all the requirements (things like postcss,
    tailwind, autoprefixer, etc) easy to setup.
    
    This demo is web-only and won't run on native. We can add native shims
    in SDK 52 with DOM components.

---
+++ with-shadcn/src/app/+html.tsx
@@ -0,0 +1,26 @@
+// import { ScrollViewStyleReset } from "expo-router/html";
+
+// This file is web-only and used to configure the root HTML for every
+// web page during static rendering.
+// The contents of this function only run in Node.js environments and
+// do not have access to the DOM or browser APIs.
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        {/* Removing the React Native Web style reset to allow for full body scrolling */}
+        {/* <ScrollViewStyleReset /> */}
+
+        {/* Add any additional <head> elements that you want globally available on web... */}
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
