9c8c58221cd104b592db63b29e58e8dd7295e56c
Aman Mittal <amandeepmittal@live.com>
2024-10-03 23:38:45 +0530

    Render statusbar before app loads in StickerSmash app

--- stickersmash/app/_layout.tsx
+++ stickersmash/app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
