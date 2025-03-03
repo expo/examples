ce3177a50bfde29f1b6903370437a369b04de933
Aman Mittal <amandeepmittal@live.com>
2025-03-04 00:52:22 +0530

    [stickersmash] Fix Modal not opening on re-adding sticker for Android (#551)
    Fix emoji picker modal not displayed on re-open on Android.

--- stickersmash/components/EmojiPicker.tsx
+++ stickersmash/components/EmojiPicker.tsx
@@ -9,17 +9,19 @@ type Props = PropsWithChildren<{
 
 export default function EmojiPicker({ isVisible, children, onClose }: Props) {
   return (
-    <Modal animationType="slide" transparent={true} visible={isVisible}>
-      <View style={styles.modalContent}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Choose a sticker</Text>
-          <Pressable onPress={onClose}>
-            <MaterialIcons name="close" color="#fff" size={22} />
-          </Pressable>
+    <View>
+      <Modal animationType="slide" transparent={true} visible={isVisible}>
+        <View style={styles.modalContent}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Choose a sticker</Text>
+            <Pressable onPress={onClose}>
+              <MaterialIcons name="close" color="#fff" size={22} />
+            </Pressable>
+          </View>
+          {children}
         </View>
-        {children}
-      </View>
-    </Modal>
+      </Modal>
+    </View>
   );
 }
 
