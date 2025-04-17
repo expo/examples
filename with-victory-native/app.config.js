c854608c6eb3d2649c400a10f55c4365f22d4f5f
evanbacon <bacon@expo.io>
2025-04-17 15:24:28 -0500

    remove extra webpack usage

--- with-victory-native/app.config.js
+++
@@ -1,7 +0,0 @@
-export default ({ config }) => ({
-  ...config,
-  splash: {
-    image:
-      "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/splash.png?raw=true",
-  },
-});
