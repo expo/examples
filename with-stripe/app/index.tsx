8044b00f1d95b2f6747f07b02ff3117455e17f70
evanbacon <bacon@expo.io>
2025-05-01 15:20:03 -0500

    update with-stripe to SDK 53

--- with-stripe/app/index.tsx
+++ with-stripe/app/index.tsx
@@ -1,16 +1,22 @@
 import CheckoutForm from "@/components/checkout-form";
 import { Image, ScrollView, Text, View } from "react-native";
 
-export default function DonatePage(): JSX.Element {
+export default function DonatePage() {
   return (
     <ScrollView
-      style={{ flex: 1, maxWidth: 600, paddingBottom: 24 }}
-      contentContainerStyle={{ padding: 16, gap: 8 }}
+      style={{
+        flex: 1,
+      }}
+      contentContainerStyle={{ padding: 16, paddingBottom: 24, gap: 8 }}
       contentInsetAdjustmentBehavior="automatic"
     >
       <Image
         source={require("@/public/react-shoe-product.jpeg")}
-        style={{ width: "100%", height: 300, borderRadius: 12 }}
+        style={{
+          width: "100%",
+          height: 300,
+          borderRadius: 12,
+        }}
       />
 
       <View>
