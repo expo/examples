cf6102b5ab6e356e35b3bf2d36dd55eef8df0a55
evanbacon <bacon@expo.io>
2025-06-27 10:02:58 -0500

    wip

---
+++ with-router-rnr/README.md
@@ -0,0 +1,16 @@
+# Expo Router and React Native Reusables
+
+Use [Expo Router](https://docs.expo.dev/router/introduction/) with [React Native Reusables](https://github.com/mrzachnugent/react-native-reusables/tree/main) components.
+
+## 🚀 How to use
+
+```sh
+npx create-expo-app -e with-router-rnr
+```
+
+## Deploy
+
+Deploy on all platforms with Expo Application Services (EAS).
+
+- Deploy the website: `npx eas-cli deploy` — [Learn more](https://docs.expo.dev/eas/hosting/get-started/)
+- Deploy on iOS and Android using: `npx eas-cli build` — [Learn more](https://expo.dev/eas)
---
+++ with-router-rnr/src/app/index.tsx
@@ -0,0 +1,95 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Page() {
+  return (
+    <View className="flex flex-1">
+      <Header />
+      <Content />
+      <Footer />
+    </View>
+  );
+}
+
+function Content() {
+  return (
+    <View className="flex-1">
+      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
+        <View className="px-4 md:px-6">
+          <View className="flex flex-col items-center gap-4 text-center">
+            <Text
+              role="heading"
+              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
+            >
+              Welcome to Project ACME
+            </Text>
+            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
+              Discover and collaborate on acme. Explore our services now.
+            </Text>
+
+            <View className="gap-4">
+              <Link
+                suppressHighlighting
+                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                href="/"
+              >
+                Explore
+              </Link>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function Header() {
+  const { top } = useSafeAreaInsets();
+  return (
+    <View style={{ paddingTop: top }}>
+      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
+        <Link className="font-bold flex-1 items-center justify-center" href="/">
+          ACME
+        </Link>
+        <View className="flex flex-row gap-4 sm:gap-6">
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            About
+          </Link>
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            Product
+          </Link>
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            Pricing
+          </Link>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function Footer() {
+  const { bottom } = useSafeAreaInsets();
+  return (
+    <View
+      className="flex shrink-0 bg-gray-100 native:hidden"
+      style={{ paddingBottom: bottom }}
+    >
+      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
+        <Text className={"text-center text-gray-700"}>
+          © {new Date().getFullYear()} Me
+        </Text>
+      </View>
+    </View>
+  );
+}
