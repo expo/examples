e7567190855e9fe943334ba2b04f3c3e9914a4f3
evanbacon <bacon@expo.io>
2025-07-01 18:29:44 -0500

    Update package.json and index.tsx to enhance dependencies and improve image handling

--- with-html/src/app/index.tsx
+++ with-html/src/app/index.tsx
@@ -1,23 +1,19 @@
-import { Image } from "expo-image";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <main className="flex-1 items-center justify-center bg-white dark:bg-slate-900 p-4">
       <article className="bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <Image
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          source={{
-            width: 384,
-            height: 512,
-            uri: "https://en.gravatar.com/userimage/120276729/078ee8361156d0e1c37b90e7851fed4b.png",
-          }}
+          className="w-24 h-24 aspect-1 rounded-full mx-auto"
+          source={{ uri: "https://github.com/evanbacon.png" }}
         />
         <main className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <p className="text-lg font-medium">
-            With <code>@expo/html-elements</code> and <code>nativewind</code>,
-            you can write HTML syntax that renders to real native components.
-            Combined with Tailwind CSS, you have an experience that makes web
-            developers feel at home.”
+          <p className="text-lg font-medium dark:text-slate-300">
+            <Icon /> With <code>@expo/html-elements</code> and{" "}
+            <code>nativewind</code>, you can write HTML syntax that renders to
+            real native components. Combined with Tailwind CSS, you have an
+            experience that makes web developers feel at home.”
           </p>
           <article className="font-medium">
             <p className="text-sky-500 dark:text-sky-400 my-0">Evan Bacon</p>
@@ -28,5 +24,25 @@ export default function App() {
         </main>
       </article>
     </main>
+  );
+}
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M6 3h12l4 6-10 13L2 9Z" />
+      <path d="M11 3 8 9l4 13 4-13-3-6" />
+      <path d="M2 9h20" />
+    </svg>
   );
 }
