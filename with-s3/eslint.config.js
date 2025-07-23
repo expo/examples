9657cdb8e48f7dc7fb44836b04a3e36b592b5082
evanbacon <bacon@expo.io>
2025-07-23 15:10:19 -0500

    reduce extra code

--- with-s3/eslint.config.js
+++
@@ -1,9 +0,0 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig, globalIgnores } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
-
-module.exports = defineConfig(globalIgnores(["dist/*"]), [
-  expoConfig,
-  eslintPluginPrettierRecommended,
-]);
