1ea2ccb82c8104e88b3a52d15c20d1b7d8037491
Evan Bacon <bacon@expo.io>
2025-11-12 16:43:11 -0800

    feat: update react-strict-dom to Expo SDK 54 (#643)
    - Delete autoprefixer — now default in Expo CLI.
    - Delete custom metro.config.js — package.json exports are now enabled
    by default.
    - Upgrade to latest RSD postcss config.
    - Use `postcss.config.mjs`.
    - Upgrade CSS directive `@stylex` -> `@react-strict-dom`.
    - Remove `EXPO_USE_FAST_RESOLVER=1` in favor of fixed default version.
    - Migrate `.stylex.ts` file to `.css.ts` file. Some strangeness here
    that requires the fully qualified path to be imported.

--- with-react-strict-dom/babel.config.js
+++ with-react-strict-dom/babel.config.js
@@ -1,5 +1,17 @@
-const { getIsDev, getPlatform } = require("babel-preset-expo/build/common");
 const reactStrictPreset = require("react-strict-dom/babel-preset");
+
+function getPlatform(caller) {
+  return caller && caller.platform;
+}
+
+function getIsDev(caller) {
+  if (caller?.isDev != null) return caller.isDev;
+  // https://babeljs.io/docs/options#envname
+  return (
+    process.env.BABEL_ENV === "development" ||
+    process.env.NODE_ENV === "development"
+  );
+}
 
 module.exports = function (api) {
   //api.cache(true);
