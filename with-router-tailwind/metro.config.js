fddd6aee0bce9fffbc6a486f883f27bf81184701
Evan Bacon <bacon@expo.io>
2025-11-29 10:55:14 -0800

    delete aliased with-router-tailwind and merge into with-tailwindcss (#647)

--- with-router-tailwind/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativewind } = require("nativewind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = withNativewind(config, {
-  inlineVariables: false,
-});
--- with-tailwindcss/metro.config.js
+++ with-tailwindcss/metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { withNativewind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+module.exports = withNativewind(config, {
+  inlineVariables: false,
+});
