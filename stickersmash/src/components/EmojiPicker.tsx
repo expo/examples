326c64323db62a61162d25be55a29c4de9d0c520
Aman Mittal <amandeepmittal@live.com>
2026-01-30 22:41:33 +0530

    [stickersmash] Update example app for SDK 55

--- stickersmash/src/components/EmojiPicker.tsx
+++ stickersmash/src/components/EmojiPicker.tsx
@@ -9,19 +9,17 @@ type Props = PropsWithChildren<{
 
 export default function EmojiPicker({ isVisible, children, onClose }: Props) {
   return (
-    <View>
-      <Modal animationType="slide" transparent={true} visible={isVisible}>
-        <View style={styles.modalContent}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Choose a sticker</Text>
-            <Pressable onPress={onClose}>
-              <MaterialIcons name="close" color="#fff" size={22} />
-            </Pressable>
-          </View>
-          {children}
+    <Modal animationType="slide" transparent={true} visible={isVisible}>
+      <View style={styles.modalContent}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Choose a sticker</Text>
+          <Pressable onPress={onClose}>
+            <MaterialIcons name="close" color="#fff" size={22} />
+          </Pressable>
         </View>
-      </Modal>
-    </View>
+        {children}
+      </View>
+    </Modal>
   );
 }
 
